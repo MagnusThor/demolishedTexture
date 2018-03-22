0649e273217a7abd79254c26c4539dd8d6baeb0a
Magnus Thor <magnus.thor74@gmail.com>
2018-03-22 12:37:05 +0100

    tysconfig changes

--- src/demolishedTexture.js
+++ src/demolishedTexture.js
@@ -1,46 +1,46 @@
-var ShaderBase = (function () {
-    function ShaderBase() {
+class ShaderBase {
+    constructor() {
         this.perm = this.seed(255);
     }
-    ShaderBase.prototype.normalize = function (a) {
-        var l = this.length(a);
+    normalize(a) {
+        let l = this.length(a);
         if (l != 0) {
             a[0] /= l;
             a[1] /= l;
             a[2] /= l;
         }
         return a;
-    };
-    ShaderBase.prototype.abs = function (a) {
-        return a.map(function (v, i) { return Math.abs(v); });
-    };
-    ShaderBase.prototype.calc = function (a, exp) { };
-    ShaderBase.prototype.toScale = function (v, w) {
+    }
+    abs(a) {
+        return a.map((v, i) => { return Math.abs(v); });
+    }
+    calc(a, exp) { }
+    toScale(v, w) {
         var a = 0, b = w, c = -1, d = 1.;
         return (v - a) / (b - a) * (d - c) + c;
-    };
+    }
     ;
-    ShaderBase.prototype.dot = function (a, b) {
+    dot(a, b) {
         return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
-    };
-    ShaderBase.prototype.gain = function (a, b) {
+    }
+    gain(a, b) {
         var c = Math.log(1 - b);
         return .001 > a ? 0 : .999 < a ? 1 : .5 > a ? Math.pow(2 * a, c) / 2 : 1 - Math.pow(2 * (1 - a), c) / 2;
-    };
-    ShaderBase.prototype.length = function (a) {
+    }
+    length(a) {
         return Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
-    };
-    ShaderBase.prototype.fade = function (t) {
+    }
+    fade(t) {
         return t * t * t * (t * (t * 6 - 15) + 10);
-    };
-    ShaderBase.prototype.lerp = function (t, a, b) { return a + t * (b - a); };
-    ShaderBase.prototype.grad = function (hash, x, y, z) {
+    }
+    lerp(t, a, b) { return a + t * (b - a); }
+    grad(hash, x, y, z) {
         var h = hash & 15;
         var u = h < 8 ? x : y, v = h < 4 ? y : h == 12 || h == 14 ? x : z;
         return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
-    };
-    ShaderBase.prototype.scale = function (n) { return (1 + n) / 2; };
-    ShaderBase.prototype.seed = function (n) {
+    }
+    scale(n) { return (1 + n) / 2; }
+    seed(n) {
         var p = [];
         for (var a = [], b = 0; n >= b; b++)
             a.push(b);
@@ -53,33 +53,31 @@ var ShaderBase = (function () {
         for (var i = 0; i < n; i++)
             p[n + i] = p[i] = a[i];
         return p;
-    };
-    ShaderBase.prototype.noise = function (x, y, z) {
-        var t = this;
-        var p = this.perm;
-        var X = ~~(x) & 255, Y = ~~(y) & 255, Z = ~~(z) & 255;
+    }
+    noise(x, y, z) {
+        let t = this;
+        let p = this.perm;
+        let X = ~~(x) & 255, Y = ~~(y) & 255, Z = ~~(z) & 255;
         x -= ~~(x);
         y -= ~~(y);
         z -= ~~(z);
-        var u = t.fade(x), v = t.fade(y), w = t.fade(z);
-        var A = p[X] + Y, AA = p[A] + Z, AB = p[A + 1] + Z, B = p[X + 1] + Y, BA = p[B] + Z, BB = p[B + 1] + Z;
+        let u = t.fade(x), v = t.fade(y), w = t.fade(z);
+        let A = p[X] + Y, AA = p[A] + Z, AB = p[A + 1] + Z, B = p[X + 1] + Y, BA = p[B] + Z, BB = p[B + 1] + Z;
         return t.scale(t.lerp(w, t.lerp(v, t.lerp(u, t.grad(p[AA], x, y, z), t.grad(p[BA], x - 1, y, z)), t.lerp(u, t.grad(p[AB], x, y - 1, z), t.grad(p[BB], x - 1, y - 1, z))), t.lerp(v, t.lerp(u, t.grad(p[AA + 1], x, y, z - 1), t.grad(p[BA + 1], x - 1, y, z - 1)), t.lerp(u, t.grad(p[AB + 1], x, y - 1, z - 1), t.grad(p[BB + 1], x - 1, y - 1, z - 1)))));
-    };
-    return ShaderBase;
-}());
-var DemolishedTextureGen = (function () {
-    function DemolishedTextureGen(width, height) {
-        var _this = this;
+    }
+}
+class DemolishedTextureGen {
+    constructor(width, height) {
         this.width = width;
         this.height = height;
-        this.coord = function (pixel, x, y, w, h, fn) {
-            var r = pixel[0];
+        this.coord = (pixel, x, y, w, h, fn) => {
+            let r = pixel[0];
             var g = pixel[1];
             var b = pixel[2];
-            var res = fn.apply(_this.helpers, [[r, b, g], x, y, w, h]);
+            var res = fn.apply(this.helpers, [[r, b, g], x, y, w, h]);
             return res;
         };
-        var c = document.createElement("canvas");
+        let c = document.createElement("canvas");
         c.width = width;
         c.height = height;
         this.ctx = c.getContext("2d");
@@ -88,14 +86,14 @@ var DemolishedTextureGen = (function () {
         this.buffer = this.ctx.getImageData(0, 0, this.width, this.height);
         this.helpers = new ShaderBase();
     }
-    DemolishedTextureGen.createTexture = function (width, height, fn) {
-        var instance = new DemolishedTextureGen(width, height);
+    static createTexture(width, height, fn) {
+        let instance = new DemolishedTextureGen(width, height);
         instance.render(fn);
         return instance.toBase64();
-    };
-    DemolishedTextureGen.prototype.render = function (fn) {
-        var buffer = this.buffer;
-        var w = this.width, h = this.height;
+    }
+    render(fn) {
+        let buffer = this.buffer;
+        let w = this.width, h = this.height;
         for (var idx, x = 0; x < w; x++) {
             for (var y = 0; y < h; y++) {
                 idx = (x + y * w) * 4;
@@ -109,9 +107,8 @@ var DemolishedTextureGen = (function () {
             }
         }
         this.ctx.putImageData(buffer, 0, 0);
-    };
-    DemolishedTextureGen.prototype.toBase64 = function () {
+    }
+    toBase64() {
         return this.ctx.canvas.toDataURL("image/png");
-    };
-    return DemolishedTextureGen;
-}());
+    }
+}
